2c436edb41cacfe77c43787b20bbe02526b4b564
Henrik Bulskov <bulskov@ruc.dk>
2023-10-12 22:49:34 +0200

    Add project files.

---
+++ Assignment3Server/MyTcpUtil.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace EchoServer;
+
+public static class MyTcpUtil
+{
+
+    public static string MyRead(this TcpClient client)
+    {
+        var stream = client.GetStream();
+        var buffer = new byte[1024];
+        var readCnt = stream.Read(buffer);
+
+        return Encoding.UTF8.GetString(buffer, 0, readCnt);
+    }
+
+    public static void MyWrite(this TcpClient client, string response)
+    {
+        var stream = client.GetStream();
+        stream.Write(Encoding.UTF8.GetBytes(response));
+    }
+}
---
+++ Assignment3Server/ServerProgram.cs
@@ -0,0 +1,31 @@
+﻿using EchoServer;
+using System.Net;
+using System.Net.Sockets;
+
+var port = 5000;
+
+var server = new TcpListener(IPAddress.Loopback, port);
+server.Start();
+
+Console.WriteLine("Server started");
+
+while(true)
+{
+    var client = server.AcceptTcpClient();
+    Console.WriteLine("Client connected");
+
+    NetworkStream stream;
+
+
+
+    string request = client.MyRead();
+
+    Console.WriteLine($"Request: {request}");
+
+    var response = request.ToUpper();
+
+    client.MyWrite(response);
+
+
+}
+
